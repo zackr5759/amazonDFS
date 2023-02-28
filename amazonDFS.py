1b90d10e97733b52b75a88c6f3b7aa82f5540b27
Zachary Robinson <51389011+zackr5759@users.noreply.github.com>
2023-02-28 15:00:00 -0800

    updated to final version

--- amazonDFS.py
+++ amazonDFS.py
@@ -1,28 +1,40 @@
-def amazonDFS(B, n, e, row, placed):
+import copy
+
+def printboard(B):
+    length = len(B)
+    for i in range(length):
+      for j in range(length):
+        if B[i] == j:
+          print("|",B[i], end="", sep='')
+        else:
+          print("|_",end="")
+        if j == length - 1:
+          print("|")
+
+def amazons(list):
+  return len(list) - list.count(-1)
+
+def amazonDFS(B):
     # B is a vector such that B[i] = the col number in which the amazon is placed in row i
     # n is the dimension of the board for which a solution is sought
     # pre-condition: B is a valid partial solution
-    e += 1
-    if count(B) == n:   # solved
-        print(B, e-1)
-        return True
+    if amazons(B) == len(B): #solved
+      return 1, B
+    count = 0
+    best = copy.deepcopy(B)
 
-    for j in range(n): #cols
-        # looking for a candidate for B[k] which will be an integer from 0 to n-1
-        if not(attack(B, n, j, row)):
-            #B.append(j)
-            if not placed[row]:#we dont want to change/remove preplaced amazons so we skip
-              B[row] = j
-
-            if amazonDFS(B, n, e, row + 1, placed):
-                return True
-            else:
-              #B.remove(j)
-              if not placed[row]:#we dont want to change/remove preplaced amazons so we skip
-                B[row] = -1
-    #print(B, e)
-    return False
-    
+    for c in range(len(B)):#rows
+      for j in range(len(B)): #cols
+        if B[c] == -1: #no amazons
+          # looking for a candidate for B[k] which will be an integer from 0 to n-1
+          if not(attack(B, len(B), j, c)):
+              B[c] = j
+              tempCount, tempB = amazonDFS(B)
+              count += tempCount
+              if amazons(tempB) > amazons(best):
+                best = copy.deepcopy(tempB)
+              B[c] = -1
+    return count+1, best
 
 def attack(B, n, j, row):
     #checks if the cell B[k+1, j] is under attack by the amazons in the first k rows of B
@@ -31,12 +43,12 @@ def attack(B, n, j, row):
         if B[k] == j and B[k] != -1:
             return True
     for i in range(rowNum):#diagonal attack
+      #print(row)
       if (B[row - i] != -1) and (B[row - i] == j - i or B[row - i] == j + i):
         return True
     for i in range(n-row):
       if (B[row + i] != -1) and (B[row + i] == j - i or B[row + i] == j + i):
         return True
-
 
 #adds knights movement
     index = rowNum - 1
@@ -58,43 +70,34 @@ def attack(B, n, j, row):
     if (index < n-2):
       if (B[index + 2] != -1) and ( B[index + 2] == j - 1 or B[index + 2] == j + 1 ):
         return True
-    
-    return False
 
-def count(B):#count how many amazons on placed
-    amazons = 0
-    for i in B:
-      if i != -1:
-        amazons += 1
-    return amazons
-        
-def preplacedAmazons(B):#save the index of preplaced amazons
-    pa = []
-    for i in B:
-      if i != -1:
-        pa.append(True)
-      else:
-        pa.append(False)
-    return pa
+    return False
 
 def main():
 #It returns a board by adding as many additional amazons as possible, 
 #return True (False) if it could find a solution with additional n − k amazons, 
 #the number of nodes expanded by DFS.
-    B = input("Enter a board as a vector B in f:")
-    B = B.strip("[]").split(",")
-    for i in range(len(B)):
-        B[i] = int(B[i])
+    print("N-Amazons program by Hector Romero and Zachary Robinson")
 
-    N = len(B)
-    placed = preplacedAmazons(B)
-#first 0 tracks expanded nodes, 
-#second 0 tracks index of B
-#placed tracks pre-placed amazons
+    while(True):
+      B = input("Enter a board as a vector B, 'q' to quit:")
+      if B == "q":
+        print("****N-Amazons program terminated****")
+        return 
 
-    amazonDFS(B, N, 0, 0, placed) 
+      B = B.strip("[]").split(",")
+      for i in range(len(B)):
+          B[i] = int(B[i])
+
+      count, best = amazonDFS(B)
+
+      if amazons(best) == len(B):
+        print(count, best, "True") # Has N (additional N-K) amazons 
+      else:
+        print(count, best, "False") # Doesn't have N amazons
+
+    #prints a 2d matrix representing the board 
+    #printboard(best) 
 
 if __name__ == "__main__":
     main()
-
-    
